a2487b975638717b774ee1f4297a6cd9bd94ff68
cale312 <zikisacalesogaxa@gmail.com>
2017-03-09 12:18:11 +0200

    first commit

--- registration_number.js
+++ registration_number.js
@@ -17,14 +17,30 @@ function add() {
     document.getElementById('text').value = "";
 }
 
-function dropDwn() {
+function filter() {
     'use strict';
-    var opt = document.getElementById("dropDwn"),
+    var opt = document.getElementById("filter").value,
         all = document.getElementById("all").innerHTML,
         cpt = document.getElementById("cpt").innerHTML,
         stell = document.getElementById("stell").innerHTML,
         paarl = document.getElementById("paarl").innerHTML;
+    if (opt === cpt) {
+        alert('filter Cape Town');
+        
+    } else if (opt === stell) {
+        alert('filter Stellenbosch');
+        
+    } else if (opt === paarl) {
+        alert('filter Paarl');
+        
+    }
 }
+
+
+//Data:
+//Paarl plate 'CJ'
+//Cape Town plate 'CA'
+//Stellenbosch plate 'CL'
 
 
 
