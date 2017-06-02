4934da5c58f54ea4b4c115d7b6b42c80b144b86c
cale312 <zikisacalesogaxa@gmail.com>
2017-06-02 16:49:46 +0200

    renamed the tested function

--- tests/filterPlates.test.js
+++ tests/filterPlates.test.js
@@ -1,19 +1,24 @@
-describe('Filtering plates by town...', function(){
+describe('Filtering plates by selected radio button', function() {
+  var data = ['CA', 'CA', 'CF', 'CL', 'CY'];
 
-  it('returns all the plates that start with "CA"', function(){
-    assert.equal('CA-123 456, CA-222 344', filterPlates('cpt', 'CA-123 456, CL-123 456, CA-222 344'));
+  it('returns all the plates that start with "CA"', function() {
+    assert.equal('CA', 'CA', filterPlates('cpt', data));
   });
 
-  it('returns all the plate that start with "CL"', function(){
-    assert.equal('CL-123 456, CL-222 333', filterPlates('stell', 'CL-123 456, CA-123 456, CL-222 333'));
+  it('returns all the plate that start with "CL"', function() {
+    assert.equal('CL', filterPlates('stell', data));
   });
 
-  it('returns all the plates that start with "CY"', function(){
-    assert.equal('CY-123 456, CY-445 666', filterPlates('bell', 'CY-123 456, CF-123 456, CY-445 666'));
+  it('returns all the plates that start with "CY"', function() {
+    assert.equal('CY', filterPlates('bell', data));
   });
 
-  it('returns all the plates that start with "CF"', function(){
-    assert.equal('CF-123 456, CF-456 789', filterPlates('kuil', 'CF-123 456, CY-123 456, CF-456 789'));
+  it('returns all the plates that start with "CF"', function() {
+    assert.equal('CF', filterPlates('kuil', data));
+  });
+
+  it('returns all the plates', function() {
+    assert.equal('CA', 'CA', 'CF', 'CL', 'CY', filterPlates('all', data));
   });
 
 });
