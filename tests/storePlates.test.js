e4c2ddb8eb4da53f10a408f3ac403ae2bd85aca8
cale312 <zikisacalesogaxa@gmail.com>
2017-06-02 16:51:32 +0200

    test for the storing of the newplate in an array

---
+++ tests/storePlates.test.js
@@ -0,0 +1,7 @@
+describe('Storing the plates in an array...', function(){
+
+  it('returns an array of the plates', function(){
+    assert.equal('CA-123 456', storePlates('CA-123 456'));
+  });
+
+});
