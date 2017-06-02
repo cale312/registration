222cebc887a8cf938f12ed76a9e25d6d7d1abf80
cale312 <zikisacalesogaxa@gmail.com>
2017-06-02 13:15:42 +0200

    called and stored the get radio button module

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,22 @@
-function add() {
-    'use strict';
-    var li, text, textNode;
-    li = document.createElement('li');
-    text = document.getElementById('text').value;
-    textNode = document.createTextNode(text.toUpperCase());
-    if (text.length > 0 && text !== " ") {
-        li.appendChild(textNode);
-        document.getElementById('lister').appendChild(li);
-    }
-    document.getElementById('text').value = "";
-}
+'use strict';
 
-function filter() {
-    'use strict';
-    var opt, all, cpt, stell, paarl, bellville, kuilsriver, list, curElem;
-    opt = document.getElementById("filter").value;
-    all = document.getElementById("all").innerHTML;
-    cpt = document.getElementById("cpt").innerHTML;
-    stell = document.getElementById("stell").innerHTML;
-    paarl = document.getElementById("paarl").innerHTML;
-    bellville = document.getElementById('bellville').innerHTML;
-    kuilsriver = document.getElementById('kuilsriver').innerHTML;
-    list = document.getElementsByTagName('li');
-    for (var i = 0; i < list.length; i++) {
-        curElem = list[i];
-        if (opt === cpt && !curElem.textContent.startsWith('CA')) {
-            curElem.style.display = 'none';
-        } else if (opt === stell & !curElem.textContent.startsWith('CL')) {
-            curElem.style.display = 'none';
-        } else if (opt === paarl && !curElem.textContent.startsWith('CJ')) {
-            curElem.style.display = 'none';
-        } else if (opt === bellville && !curElem.textContent.startsWith('CY')) {
-            curElem.style.display = 'none';
-        } else if (opt === kuilsriver && !curElem.textContent.startsWith('CF')) {
-            curElem.style.display = 'none';
-        }else if (opt === all || opt === "") {
-            curElem.style.display = 'inline-block';
-        } else {
-            curElem.style.display = 'inline-block';
-        }
-    }
-}
+var newPlate = document.querySelector('.plate-number');
+var addBtn = document.querySelector('.add-btn');
+var filterBtn = document.querySelector('.filter-btn');
+var displayPlates = document.querySelector('.plate-list');
+var platesWrapper = document.querySelector('.plates-wrapper');
+var radioButtons = document.getElementsByName('town');
+
+addBtn.addEventListener('click', function(){
+  var thePlate = getPlate(newPlate.value);
+  var appendPlate = createLi(thePlate);
+  if (appendPlate !== undefined){
+    displayPlates.appendChild(appendPlate);
+  }
+  newPlate.value = '';
+});
+
+filterBtn.addEventListener('click', function(){
+  var checkedButton = getRadioButton(radioButtons);
+  // var filterResults  = filterPlates(checkedButton);
+});
