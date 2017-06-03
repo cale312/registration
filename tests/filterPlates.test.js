a589ab89896437e69c602f8b98b885bea29f86b0
cale312 <zikisacalesogaxa@gmail.com>
2017-06-03 07:22:19 +0200

    test module for the filtering of the plates

--- tests/filterPlates.test.js
+++ tests/filterPlates.test.js
@@ -1,23 +1,23 @@
 describe('Filtering plates by selected radio button', function() {
   var data = ['CA', 'CA', 'CF', 'CL', 'CY'];
 
-  it('returns all the plates that start with "CA"', function() {
+  it('returns all the plates that start with "CA" if radio button Cape Town is checked', function() {
     assert.equal('CA', 'CA', filterPlates('cpt', data));
   });
 
-  it('returns all the plate that start with "CL"', function() {
+  it('returns all the plate that start with "CL" if radio button Stellenbosch is checked', function() {
     assert.equal('CL', filterPlates('stell', data));
   });
 
-  it('returns all the plates that start with "CY"', function() {
+  it('returns all the plates that start with "CY" if radio button Bellville is checked', function() {
     assert.equal('CY', filterPlates('bell', data));
   });
 
-  it('returns all the plates that start with "CF"', function() {
+  it('returns all the plates that start with "CF" if radio button Kuilsriver is checked', function() {
     assert.equal('CF', filterPlates('kuil', data));
   });
 
-  it('returns all the plates', function() {
+  it('returns all the plates if no radio button or the all button is checked', function() {
     assert.equal('CA', 'CA', 'CF', 'CL', 'CY', filterPlates('all', data));
   });
 
