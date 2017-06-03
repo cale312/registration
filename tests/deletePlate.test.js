6f80761bf0623d1fcf1581d54b146064d52a1f58
cale312 <zikisacalesogaxa@gmail.com>
2017-06-03 09:30:02 +0200

    dleted scripts

--- tests/deletePlate.test.js
+++
@@ -1,8 +0,0 @@
-describe('Deletes the plate selected...', function(){
-  var data = ['CA-123 456', 'CA-654 321', 'CF-123 456', 'CL-123 456', 'CY-123 456'];
-
-  it('deletes the selected plate and returns the new array list', function(){
-    assert.equal('CA-654 321', 'CF-123 456', 'CL-123 456', 'CY-123 456', deletePlate('CA-123 456', data));
-  });
-
-});
